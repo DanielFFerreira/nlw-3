8c879820240f0b9fa5c25e2686d0ac309756684a
DanielFerreira <danielfferreirabr@gmail.com>
2020-10-16 02:56:43 -0300

    Orphanage page completed

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -24,3 +24,29 @@ const icon = L.icon({
 L
 .marker([-20.9417271,-48.4910651], { icon })
 .addTo(map)
+
+
+// image gallery
+
+function selectImage(event) {
+  const button = event.currentTarget
+
+  //remover todas as classes .active 
+  const buttons = document.querySelectorAll(".images button")
+  buttons.forEach((button) => {
+    button.classList.remove("active")
+  })
+
+  //selecionar imagem clicada
+  const image = button.children[0]
+  const imageContainer = document.querySelector(".orphanage-details > img")
+
+
+  //atualizar o container de imagem
+  imageContainer.src = image.src
+
+  
+  //adicionar a classe active para este botao
+  button.classList.add('active')
+
+}
