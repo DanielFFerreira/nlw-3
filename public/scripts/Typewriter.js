06b6a26e6d8ab30b6a5d3d2bd3c2d3a3dc2d9d6f
DanielFerreira <danielfferreirabr@gmail.com>
2020-10-17 14:46:28 -0300

    Adding script animations

---
+++ public/scripts/Typewriter.js
@@ -0,0 +1,15 @@
+function typeWrite(elemento){
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = ' ';
+  textoArray.forEach(function(letra, i){   
+    
+  setTimeout(function(){
+      elemento.innerHTML += letra;
+  }, 75 * i)
+
+});
+}
+const titulo = document.querySelector('h1');
+typeWrite(titulo);
+
+typeWrite(document.querySelector('#animate-up'));
