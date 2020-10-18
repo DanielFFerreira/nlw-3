2fea72a2ca881fc79f3634f6c5256e12eb82b86a
DanielFerreira <danielfferreirabr@gmail.com>
2020-10-18 18:45:24 -0300

    finished script

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -1,55 +1,73 @@
 //create map
-const map = L.map('mapid').setView([-20.9374971,-48.4959839], 15);
+const map = L.map("mapid").setView([-27.2109325, -49.6448719], 15);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
-.addTo(map);
-
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29,68],
-})
+  iconUrl: "/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+});
 
 let marker;
-// create and add markers
-map.on('click', (event) => {
+
+map.on("click", (event) => {
   const lat = event.latlng.lat;
-  const lng = event.latlng.lng; 
+  const lng = event.latlng.lng;
 
-  document.querySelector('[name=lat]').value = lat;
-  document.querySelector('[name=lat]').value = lng;
+  document.querySelector("[name=lat]").value = lat;
+  document.querySelector("[name=lng]").value = lng;
 
-  // remover icon
   marker && map.removeLayer(marker);
 
-  //add icon tileLayer
-  marker = L.marker([lat, lng], {icon})
-  .addTo(map)
-})
+  marker = L.marker([lat, lng], { icon }).addTo(map);
+});
 
-
-// add camp of foto
 function addPhotoField() {
-  const container = document.querySelector('#images')
-  const newimages = document.querySelectorAll('.new-upload')
-  const newfieldContainer = newimages[newimages.length - 1].cloneNode(true)
-  const input = newfieldContainer.children[0]
-  if(input.value == "") {
-    return
+  const container = document.querySelector("#images");
+
+  const fieldsContainer = document.querySelectorAll(".new-upload");
+
+  const newFieldContainer = fieldsContainer[
+    fieldsContainer.length - 1
+  ].cloneNode(true);
+
+  const input = newFieldContainer.children[0];
+
+  if (input.value == "") {
+    return;
   }
-  input.value = ""
-  container.appendChild(newfieldContainer)
+
+  input.value = "";
+
+  container.appendChild(newFieldContainer);
 }
 
 function deleteField(event) {
-  const span = event.currentTarget
-  const fieldContainer = document.querySelectorAll('.new-upload')
+  const span = event.currentTarget;
 
-  if(fieldContainer.length <= 1) {
-    span.parentNode.children[0].value = ""
-    return
+  const fieldsContainer = document.querySelectorAll(".new-upload");
+
+  if (fieldsContainer.length <= 1) {
+    span.parentNode.children[0].value = "";
+    return;
   }
 
   span.parentNode.remove();
+}
+
+function toggleSelect(event) {
+  document
+    .querySelectorAll(".button-select button")
+    .forEach((button) => button.classList.remove("active"));
+
+  const button = event.currentTarget;
+  button.classList.add("active");
+
+  const input = document.querySelector('[name="open_on_weekends"]');
+  input.value = button.dataset.value;
+}
+
+function validade(event) {
+  // event.preventDefault()
 }
